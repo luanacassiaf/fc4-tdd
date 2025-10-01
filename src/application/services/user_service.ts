d48924828fb527e157fc99966aeca9146bd27870
luanacassiaf <luanacassiaf@gmail.com>
2025-10-01 16:19:53 -0300

    feat: create user

--- src/application/services/user_service.ts
+++ src/application/services/user_service.ts
@@ -1,8 +1,21 @@
 import { User } from "../../domain/entities/user";
 import { UserRepository } from "../../domain/repositories/user_repository";
+import { v4 as uuidv4 } from "uuid";
+import { CreateUserDTO } from "../dtos/create_user_dto";
 
 export class UserService {
-  constructor(private readonly userRepository: UserRepository) {}
+  constructor(private readonly userRepository: UserRepository) { }
+
+  async createUser(dto: CreateUserDTO): Promise<User> {
+    const user = new User(
+      uuidv4(),
+      dto.name
+    );
+
+    await this.userRepository.save(user);
+    return user;
+  }
+
   async findUserById(id: string): Promise<User | null> {
     return this.userRepository.findById(id);
   }
---
+++ src/infrastructure/web/user_controller.ts
@@ -0,0 +1,30 @@
+import { Request, Response } from "express";
+import { UserService } from "../../application/services/user_service";
+import { CreateUserDTO } from "../../application/dtos/create_user_dto";
+
+export class UserController {
+	private userService: UserService;
+
+	constructor(userService: UserService) {
+		this.userService = userService;
+	}
+
+	async createUser(req: Request, res: Response): Promise<Response> {
+		try {
+			const dto: CreateUserDTO = {
+				name: req.body.name
+			};
+
+			const user = await this.userService.createUser(dto);
+
+			return res.status(201).json({
+				message: "Usuário criado com sucesso",
+				booking: user,
+			});
+		} catch (error: any) {
+			return res
+				.status(400)
+				.json({ message: error.message || "An unexpected error occurred" });
+		}
+	}
+}
